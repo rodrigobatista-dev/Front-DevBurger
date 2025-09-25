7a53272b007c7e28427df931584f3795ae3d9a53
rodrigobatista-dev <rodrigobatista03@gmail.com>
2025-09-25 15:46:22 -0300

    adicionardo carrossel de categorias com react-multi-carousel

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -1,26 +1,60 @@
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from 'react'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
 
 import api from '../../services/api'
+import { Container, ContainerItems, Title } from './styles'
 
 export function CategoriesCarousel() {
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([])
 
   useEffect(() => {
     async function loadCategoris() {
       const { data } = await api.get('/categories')
 
-      setCategories(data);
-      console.log(data);
+      setCategories(data)
+      console.log(data)
     }
-    
-    loadCategoris()
-  }, []);
 
+    loadCategoris()
+  }, [])
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1280 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1280, min: 690 },
+      items: 3,
+    },
+    mobile: {
+      breakpoint: { max: 690, min: 0 },
+      items: 2,
+    },
+  }
 
   return (
-    <div>
-      <h1>OK</h1>
-    </div>
+    <Container>
+      <Title>Categorias</Title>
+
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        partialVisible={false}
+        itemClass="carousel-item"
+      >
+        {categories.map((category) => (
+          <ContainerItems key={category.id} imageUrl={category.url}>
+            <p>{category.name}</p>
+          </ContainerItems>
+        ))}
+      </Carousel>
+    </Container>
   )
 }
