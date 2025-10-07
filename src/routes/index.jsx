05df4d7553f4615a895e2bdd56d72bf68624dec4
rodrigobatista-dev <rodrigobatista03@gmail.com>
2025-10-07 12:09:36 -0300

    fix: define rota padrão para login

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,18 +1,25 @@
 /* eslint-disable no-unused-vars */
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
 
+import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 import { Home } from '../containers/Home'
 import { Login } from '../containers/Login'
 import { Menu } from '../containers/Menu'
 import { Register } from '../containers/Register'
+
 export const router = createBrowserRouter([
   {
     path: '/',
+    element: <Navigate to="/login" replace />,
+  },
+  {
+    path: '/home',
     element: (
       <>
         <Header />
         <Home />
+        <Footer />
       </>
     ),
   },
